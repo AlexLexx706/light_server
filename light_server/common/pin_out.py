93f3207a9266aeb6669a4c3193f2a0e4e5003b9d
alex <akalmykov@topcon.com>
2023-05-19 03:40:46 +0200

    update

--- light_server/common/pin_out.py
+++ light_server/common/pin_out.py
@@ -5,21 +5,35 @@ import logging
 
 LOG = logging.getLogger(__name__)
 
+
 class PinOut:
     """gpio pin
     """
-    def __init__(self, pin_num):
+    VALUE_MAP = {0: 'off', 1: 'on'}
+
+    def __init__(self, pin_num, name):
         self._num = pin_num
+        self._val = 0
+        self._name = name
         try:
             with open('/sys/class/gpio/export', 'w', encoding="utf8") as f:
                 f.write(f'{self._num}')
             with open(f'/sys/class/gpio/gpio{self._num}/direction', 'w', encoding="utf8") as file:
                 file.write('out')
-            self._file = open(f'/sys/class/gpio/gpio{self._num}/value', 'w', encoding="utf8")
+            self._file = open(
+                f'/sys/class/gpio/gpio{self._num}/value', 'w', encoding="utf8")
         except Exception as exc:
             LOG.warning("Can't open gpio:%s", exc)
             self._file = None
         self.set(1)
+
+    @property
+    def value(self):
+        return self.VALUE_MAP[self._val]
+
+    @property
+    def name(self):
+        return self._name
 
     def set(self, val):
         """set pin value
@@ -27,11 +41,15 @@ class PinOut:
         Args:
             val (int): 0/1
         """
+        LOG.info('set:%s', val)
+
         if self._file:
             self._file.write(f'{val}')
             self._file.flush()
         else:
             LOG.warning("No gpio:%s", self._num)
+        self._val = val
+
 
 if __name__ == '__main__':
     pin = PinOut(14)
--- light_server/server.py
+++ light_server/server.py
@@ -1,14 +1,35 @@
-from gevent.pywsgi import WSGIServer
 from flask import Flask, request, json
 from flask import render_template
+from flask_socketio import SocketIO, send, emit
 from light_server.common import pin_out
+import logging
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SECRET_KEY'] = 'secret!'
+app.config["DEBUG"] = True
+
+socketio = SocketIO(app)
+
+# CONTROL pins!!!
 pins = {
-    0: pin_out.PinOut(14),
-    1: pin_out.PinOut(15)}
-COUNTER = 0
+    0: pin_out.PinOut(14, 'Timer enable'),
+    1: pin_out.PinOut(15, 'Power'),
+    2: pin_out.PinOut(14, 'Timer enable AGI2 rev6.11'),
+    3: pin_out.PinOut(15, 'Power AGI2 rev6.11'),
+    4: pin_out.PinOut(14, 'Timer enable, хз что'),
+    5: pin_out.PinOut(15, 'Power, хз что'),
+}
+
+@socketio.on('value_changed')
+def handle_value_changed(data):
+    """hendler for client data
+    Args:
+        data (json): client data:{'id':int, 'value':0/1}
+    """
+    app.logger.info('received json: %s', str(data))
+    pins[data['id']].set(data['value'])
+    emit('update_value', data, broadcast=True)
 
 
 @app.route("/")
@@ -18,30 +39,20 @@ def hello():
     Returns:
         string: html page
     """
-    return render_template('hello.html', counter=COUNTER)
+    buttons = [{'id': key, 'value': pin.value, 'name': pin.name}
+               for key, pin in pins.items()]
+    app.logger.info('hello: %s', buttons)
 
-
-@app.route('/api/light', methods=['PUT'])
-def light():
-    """change GPIO state
-    Returns:
-        string: json string with result of changing ping state
-    """
-    data = request.get_json()
-    try:
-        pins[data['id']].set(data['value'])
-        return json.dumps({'res': "ok"})
-    except KeyError:
-        json.dumps({'res': f"wrong light id:{data['id']}"})
+    return render_template(
+        'hello.html',
+        buttons=buttons)
 
 
 def main():
     """Server
     """
     port = 8080
-    print("start server on port:{port}")
-    http_server = WSGIServer(('', port), app)
-    http_server.serve_forever()
+    socketio.run(app, host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
